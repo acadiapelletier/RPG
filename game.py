4969682ba6fed3b2bf95619bfa745f3ffb35f44c
acadiapelletier <60437226+acadiapelletier@users.noreply.github.com>
2021-01-18 09:45:36 -0600

    Create game.py

---
+++ game.py
@@ -0,0 +1,12 @@
+items = ['Key' , 'Magic Wand' , 'Secret Spellbook']
+spells = ['Flame Thrower', 'Special Attack', 'Final Blow']
+
+## Prints off available items in the inventory
+print('Available items:')
+for i, item in enumerate(items, 1):
+  print(f"{i}. {item}")
+
+## Prints off available spells in the inventory
+print('Available spells')
+for i, spell in enumerate(spells, 1):
+  print(f"{i}. {spell}")
